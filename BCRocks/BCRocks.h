cf4e1c5bafcf7e717fcc474dc62418e5d50a172d
iska <iska.gp@gmail.com>
2014-11-15 17:12:03 +0100

    Add Xcode project

---
+++ BCRocks/BCRocks.h
@@ -0,0 +1,13 @@
+//
+//  BCRocks.h
+//  BCRocks
+//
+//  Created by Iska on 15/11/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BCRocks : NSObject
+
+@end
---
+++ BCRocks/BCRocks.m
@@ -0,0 +1,13 @@
+//
+//  BCRocks.m
+//  BCRocks
+//
+//  Created by Iska on 15/11/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import "BCRocks.h"
+
+@implementation BCRocks
+
+@end
