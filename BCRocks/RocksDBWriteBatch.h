9a1487079e8d76f85874ab8f853e5fd735a03f71
iska <iska.gp@gmail.com>
2014-12-02 00:41:35 +0100

    Add wrapper for WriteBatch class

---
+++ BCRocks/RocksDBWriteBatch.h
@@ -0,0 +1,13 @@
+//
+//  RocksDBWriteBatch.h
+//  BCRocks
+//
+//  Created by Iska on 02/12/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RocksDBWriteBatch : NSObject
+
+@end
---
+++ BCRocks/RocksDBWriteBatch.mm
@@ -0,0 +1,13 @@
+//
+//  RocksDBWriteBatch.m
+//  BCRocks
+//
+//  Created by Iska on 02/12/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import "RocksDBWriteBatch.h"
+
+@implementation RocksDBWriteBatch
+
+@end
