ed7e3810be706974d69023e7d24ae68ae48a4af4
iska <iska.gp@gmail.com>
2014-12-03 00:27:14 +0100

    Add test cases for write batch delete and clear operations

--- BCRocksTests/RocksDBWriteBatchTests.mm
+++ BCRocksTests/RocksDBWriteBatchTests.mm
@@ -45,8 +45,7 @@
 	[super tearDown];
 }
 
-
-- (void)testExample
+- (void)testWriteBatch
 {
 	[_rocks performWriteBatch:^(RocksDBWriteBatch *batch, RocksDBWriteOptions *options) {
 		[batch setData:Data(@"Value 1") forKey:Data(@"Key 1")];
@@ -58,6 +57,40 @@
 	XCTAssertEqualObjects([_rocks dataForKey:Data(@"Key 2")], Data(@"Value 2"));
 	XCTAssertEqualObjects([_rocks dataForKey:Data(@"Key 3")], Data(@"Value 3"));
 	XCTAssertEqualObjects([_rocks dataForKey:Data(@"Key 4")], nil);
+}
+
+- (void)testWriteBatch_DeleteOps
+{
+	[_rocks setData:Data(@"Value 1") forKey:Data(@"Key 1")];
+
+	[_rocks performWriteBatch:^(RocksDBWriteBatch *batch, RocksDBWriteOptions *options) {
+		[batch deleteDataForKey:Data(@"Key 1")];
+		[batch setData:Data(@"Value 2") forKey:Data(@"Key 2")];
+		[batch setData:Data(@"Value 3") forKey:Data(@"Key 3")];
+	}];
+
+	XCTAssertEqualObjects([_rocks dataForKey:Data(@"Key 1")], nil);
+	XCTAssertEqualObjects([_rocks dataForKey:Data(@"Key 2")], Data(@"Value 2"));
+	XCTAssertEqualObjects([_rocks dataForKey:Data(@"Key 3")], Data(@"Value 3"));
+	XCTAssertEqualObjects([_rocks dataForKey:Data(@"Key 4")], nil);
+}
+
+- (void)testWriteBatch_ClearOps
+{
+	[_rocks setData:Data(@"Value 1") forKey:Data(@"Key 1")];
+
+	[_rocks performWriteBatch:^(RocksDBWriteBatch *batch, RocksDBWriteOptions *options) {
+		[batch deleteDataForKey:Data(@"Key 1")];
+		[batch setData:Data(@"Value 2") forKey:Data(@"Key 2")];
+		[batch setData:Data(@"Value 3") forKey:Data(@"Key 3")];
+		[batch clear];
+		[batch setData:Data(@"Value 4") forKey:Data(@"Key 4")];
+	}];
+
+	XCTAssertEqualObjects([_rocks dataForKey:Data(@"Key 1")], Data(@"Value 1"));
+	XCTAssertEqualObjects([_rocks dataForKey:Data(@"Key 2")], nil);
+	XCTAssertEqualObjects([_rocks dataForKey:Data(@"Key 3")], nil);
+	XCTAssertEqualObjects([_rocks dataForKey:Data(@"Key 4")], Data(@"Value 4"));
 }
 
 @end
