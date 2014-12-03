d03fdc19141b48576f4c9c6979f9891a53c84146
iska <iska.gp@gmail.com>
2014-12-04 00:13:18 +0100

    Add class for Comparator tests

---
+++ BCRocksTests/RocksDBComparatorTests.mm
@@ -0,0 +1,62 @@
+//
+//  RocksDBComparatorTests.m
+//  BCRocks
+//
+//  Created by Iska on 04/12/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "BCRocks.h"
+
+#define Data(x) [x dataUsingEncoding:NSUTF8StringEncoding]
+#define Str(x)	[[NSString alloc] initWithData:x encoding:NSUTF8StringEncoding]
+
+@interface RocksDBComparatorTests : XCTestCase
+{
+	NSString *_path;
+	RocksDB *_rocks;
+}
+@end
+
+@implementation RocksDBComparatorTests
+
+- (void)setUp
+{
+	[super setUp];
+
+	_path = [[NSBundle bundleForClass:[self class]] resourcePath];
+	_path = [_path stringByAppendingPathComponent:@"BCRocks"];
+}
+
+- (void)tearDown
+{
+	NSString * path = [[NSBundle bundleForClass:[self class]] resourcePath];
+	path = [path stringByAppendingPathComponent:@"BCRocks"];
+
+	NSError *error = nil;
+	[[NSFileManager defaultManager] removeItemAtPath:path error:&error];
+	if (error) {
+		NSLog(@"Error test teardown: %@", [error debugDescription]);
+	}
+	[super tearDown];
+}
+
+- (void)testDB_Comparator
+{
+	_rocks = [[RocksDB alloc] initWithPath:_path andDBOptions:^(RocksDBOptions *options) {
+		options.createIfMissing = YES;
+		options.comparator = ^ int (NSData *data1, NSData *data2) {
+			return [Str(data1) compare:Str(data2)];
+		};
+	}];
+
+	NSMutableArray *expected = [NSMutableArray array];
+	for (int i = 0; i < 26; i++) {
+		NSString *str = [NSString stringWithFormat:@"A%d", i];
+		[expected addObject:str];
+		[_rocks setData:Data(str) forKey:Data(str)];
+	}
+}
+
+@end
