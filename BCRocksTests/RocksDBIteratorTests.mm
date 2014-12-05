bd4ae998657f32c4cb2ae399d202bd3d6cdea693
iska <iska.gp@gmail.com>
2014-12-05 23:03:19 +0100

    Add tests for RocksDB Iterator

--- BCRocksTests/RocksDBIteratorTests.mm
+++ BCRocksTests/RocksDBIteratorTests.mm
@@ -52,13 +52,112 @@
 	[_rocks setData:Data(@"value 2") forKey:Data(@"key 2")];
 	[_rocks setData:Data(@"value 3") forKey:Data(@"key 3")];
 
-	NSMutableArray *expected = [NSMutableArray array];
-	[_rocks enumerateKeysUsingBlock:^(id key, BOOL *stop) {
-		[expected addObject:Str(key)];
+	NSMutableArray *actual = [NSMutableArray array];
+	RocksDBIterator *iterator = [_rocks iterator];
+	for ([iterator seekToFirst]; [iterator isValid]; [iterator next]) {
+		[actual addObject:Str([iterator key])];
+	}
+
+	NSArray *expected = @[ @"key 1", @"key 2", @"key 3" ];
+	XCTAssertEqualObjects(actual, expected);
+
+	[iterator close];
+	[_rocks close];
+}
+
+- (void)testDB_Iterator_Reverse
+{
+	_rocks = [[RocksDB alloc] initWithPath:_path andDBOptions:^(RocksDBOptions *options) {
+		options.createIfMissing = YES;
 	}];
 
-	NSArray *actual = @[ @"key 1", @"key 2", @"key 3" ];
+	[_rocks setData:Data(@"value 1") forKey:Data(@"key 1")];
+	[_rocks setData:Data(@"value 2") forKey:Data(@"key 2")];
+	[_rocks setData:Data(@"value 3") forKey:Data(@"key 3")];
+
+	NSMutableArray *actual = [NSMutableArray array];
+	RocksDBIterator *iterator = [_rocks iterator];
+	[iterator enumerateKeysInReverse:YES usingBlock:^(id key, BOOL *stop) {
+		[actual addObject:Str(key)];
+	}];
+
+	NSArray *expected = @[ @"key 3", @"key 2", @"key 1" ];
 	XCTAssertEqualObjects(actual, expected);
+
+	[iterator close];
+	[_rocks close];
+}
+
+- (void)testDB_Iterator_RangeStart
+{
+	_rocks = [[RocksDB alloc] initWithPath:_path andDBOptions:^(RocksDBOptions *options) {
+		options.createIfMissing = YES;
+	}];
+
+	[_rocks setData:Data(@"value 1") forKey:Data(@"key 1")];
+	[_rocks setData:Data(@"value 2") forKey:Data(@"key 2")];
+	[_rocks setData:Data(@"value 3") forKey:Data(@"key 3")];
+	[_rocks setData:Data(@"value 4") forKey:Data(@"key 4")];
+
+	NSMutableArray *actual = [NSMutableArray array];
+	RocksDBIterator *iterator = [_rocks iterator];
+	[iterator enumerateKeysInRange:RocksMakeRange(Data(@"key 2"), nil) reverse:NO usingBlock:^(id key, BOOL *stop) {
+		[actual addObject:Str(key)];
+	}];
+
+	NSArray *expected = @[ @"key 2", @"key 3", @"key 4" ];
+	XCTAssertEqualObjects(actual, expected);
+
+	[iterator close];
+	[_rocks close];
+}
+
+- (void)testDB_Iterator_RangeEnd
+{
+	_rocks = [[RocksDB alloc] initWithPath:_path andDBOptions:^(RocksDBOptions *options) {
+		options.createIfMissing = YES;
+	}];
+
+	[_rocks setData:Data(@"value 1") forKey:Data(@"key 1")];
+	[_rocks setData:Data(@"value 2") forKey:Data(@"key 2")];
+	[_rocks setData:Data(@"value 3") forKey:Data(@"key 3")];
+	[_rocks setData:Data(@"value 4") forKey:Data(@"key 4")];
+
+	NSMutableArray *actual = [NSMutableArray array];
+	RocksDBIterator *iterator = [_rocks iterator];
+	[iterator enumerateKeysInRange:RocksMakeRange(nil, Data(@"key 4")) reverse:NO usingBlock:^(id key, BOOL *stop) {
+		[actual addObject:Str(key)];
+	}];
+
+	NSArray *expected = @[ @"key 1", @"key 2", @"key 3" ];
+	XCTAssertEqualObjects(actual, expected);
+
+	[iterator close];
+	[_rocks close];
+}
+
+- (void)testDB_Iterator_RangeStartEnd
+{
+	_rocks = [[RocksDB alloc] initWithPath:_path andDBOptions:^(RocksDBOptions *options) {
+		options.createIfMissing = YES;
+	}];
+
+	[_rocks setData:Data(@"value 1") forKey:Data(@"key 1")];
+	[_rocks setData:Data(@"value 2") forKey:Data(@"key 2")];
+	[_rocks setData:Data(@"value 3") forKey:Data(@"key 3")];
+	[_rocks setData:Data(@"value 4") forKey:Data(@"key 4")];
+
+	NSMutableArray *actual = [NSMutableArray array];
+	RocksDBIterator *iterator = [_rocks iterator];
+	[iterator enumerateKeysInRange:RocksMakeRange(Data(@"key 2"), Data(@"key 4")) reverse:NO usingBlock:^(id key, BOOL *stop) {
+		[actual addObject:Str(key)];
+	}];
+
+	NSArray *expected = @[ @"key 2", @"key 3" ];
+	XCTAssertEqualObjects(actual, expected);
+
+	[iterator close];
+	[_rocks close];
 }
 
 @end
