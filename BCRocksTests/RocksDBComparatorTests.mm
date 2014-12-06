40fccaba90db1a9ad86729e59e4da3ff04cab520
iska <iska.gp@gmail.com>
2014-12-06 01:05:18 +0100

    Add tests for RocksDB keys comparator
    String compare, String length asc, String length desc

--- BCRocksTests/RocksDBComparatorTests.mm
+++ BCRocksTests/RocksDBComparatorTests.mm
@@ -41,7 +41,7 @@
 	[super tearDown];
 }
 
-- (void)testDB_Comparator
+- (void)testDB_Comparator_StringCompare
 {
 	_rocks = [[RocksDB alloc] initWithPath:_path andDBOptions:^(RocksDBOptions *options) {
 		options.createIfMissing = YES;
@@ -50,12 +50,77 @@
 		};
 	}];
 
+	/* Expected Array: [A0, A1, A11, A12 ... A2, A21 ...] */
 	NSMutableArray *expected = [NSMutableArray array];
 	for (int i = 0; i < 26; i++) {
 		NSString *str = [NSString stringWithFormat:@"A%d", i];
 		[expected addObject:str];
 		[_rocks setData:Data(str) forKey:Data(str)];
 	}
+	[expected sortUsingSelector:@selector(compare:)];
+
+	__block NSUInteger idx = 0;
+	RocksDBIterator *iterator = [_rocks iterator];
+	[iterator enumerateKeysUsingBlock:^(id key, BOOL *stop) {
+		XCTAssertEqualObjects(Str(key), expected[idx]);
+		idx++;
+	}];
+}
+
+- (void)testDB_Comparator_StringLengthCompare_Asc
+{
+	_rocks = [[RocksDB alloc] initWithPath:_path andDBOptions:^(RocksDBOptions *options) {
+		options.createIfMissing = YES;
+		options.comparator = ^ int (NSData *data1, NSData *data2) {
+			if (Str(data1).length > Str(data2).length) return 1;
+			if (Str(data1).length < Str(data2).length) return -1;
+			return 0;
+		};
+	}];
+
+	/* Expected Array: [A, BB, CCC, ... , Y{25}, Z{26}] */
+	NSMutableArray *expected = [NSMutableArray array];
+	for (unichar i = 65; i <= 90; i++) {
+		NSString *str = [NSString stringWithCharacters:&i length:1];
+		str = [str stringByPaddingToLength:(i-64) withString:str startingAtIndex:0];
+		[expected addObject:str];
+		[_rocks setData:Data(str) forKey:Data(str)];
+	}
+
+	__block NSUInteger idx = 0;
+	RocksDBIterator *iterator = [_rocks iterator];
+	[iterator enumerateKeysUsingBlock:^(id key, BOOL *stop) {
+		XCTAssertEqualObjects(Str(key), expected[idx]);
+		idx++;
+	}];
+}
+
+- (void)testDB_Comparator_StringLengthCompare_Desc
+{
+	_rocks = [[RocksDB alloc] initWithPath:_path andDBOptions:^(RocksDBOptions *options) {
+		options.createIfMissing = YES;
+		options.comparator = ^ int (NSData *data1, NSData *data2) {
+			if (Str(data1).length > Str(data2).length) return -1;
+			if (Str(data1).length < Str(data2).length) return 1;
+			return 0;
+		};
+	}];
+
+	/* Expected Array: [Z{26}, Y{25}, ..., CCC, BB, A] */
+	NSMutableArray *expected = [NSMutableArray array];
+	for (unichar i = 90; i >= 65; i--) {
+		NSString *str = [NSString stringWithCharacters:&i length:1];
+		str = [str stringByPaddingToLength:(i-64) withString:str startingAtIndex:0];
+		[expected addObject:str];
+		[_rocks setData:Data(str) forKey:Data(str)];
+	}
+
+	__block NSUInteger idx = 0;
+	RocksDBIterator *iterator = [_rocks iterator];
+	[iterator enumerateKeysUsingBlock:^(id key, BOOL *stop) {
+		XCTAssertEqualObjects(Str(key), expected[idx]);
+		idx++;
+	}];
 }
 
 @end
