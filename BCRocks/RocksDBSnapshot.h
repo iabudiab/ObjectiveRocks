e486c7d4392a112b63f50b909ff3975ad477a4f8
iska <iska.gp@gmail.com>
2014-12-06 21:52:01 +0100

    Add class stubs for RocksDB Snapshot

---
+++ BCRocks/RocksDBSnapshot.h
@@ -0,0 +1,13 @@
+//
+//  RocksDBSnapshot.h
+//  BCRocks
+//
+//  Created by Iska on 06/12/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RocksDBSnapshot : NSObject
+
+@end
---
+++ BCRocks/RocksDBSnapshot.mm
@@ -0,0 +1,13 @@
+//
+//  RocksDBSnapshot.m
+//  BCRocks
+//
+//  Created by Iska on 06/12/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#import "RocksDBSnapshot.h"
+
+@implementation RocksDBSnapshot
+
+@end
