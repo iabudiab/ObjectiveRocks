a208370ccd6810577f6406b7510910b2999479eb
iska <iska.gp@gmail.com>
2014-12-06 21:53:36 +0100

    Add interface definition for RocksDB Snapshot
    The snapshot is a read-only subclass of the main DB class, this allows for method and logic reuse without duplication.
    All write/delete ops are marked as unavailable to generate compiler errors if used.

--- BCRocks/RocksDBSnapshot.h
+++ BCRocks/RocksDBSnapshot.h
@@ -7,7 +7,56 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RocksDB.h"
+#import "RocksDBReadOptions.h"
 
-@interface RocksDBSnapshot : NSObject
+namespace rocksdb {
+	class DB;
+}
+
+@interface RocksDBSnapshot : RocksDB
+
+- (instancetype)initWithDBInstance:(rocksdb::DB *)db andReadOptions:(RocksDBReadOptions *)readOptions;
+
+@end
+
+#define NA(x) __attribute__((unavailable(x)))
+
+@interface RocksDBSnapshot (Unavailable)
+
+- (instancetype)initWithPath:(NSString *)path NA("Create a snapshot via a DB instance");
+- (instancetype)initWithPath:(NSString *)path
+				andDBOptions:(void (^)(RocksDBOptions *options))options NA("Create a snapshot via a DB instance");
+
+- (void)setDefaultReadOptions:(void (^)(RocksDBReadOptions *readOptions))readOptions
+			  andWriteOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Specify options when creating snapshot via DB instance");
+
+- (BOOL)setData:(NSData *)data
+		 forKey:(NSData *)aKey NA("Snapshots are read-only");
+- (BOOL)setData:(NSData *)data
+		 forKey:(NSData *)aKey
+		  error:(NSError **)error NA("Snapshots are read-only");
+- (BOOL)setData:(NSData *)data
+		 forKey:(NSData *)aKey
+   writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Snapshots are read-only");
+- (BOOL)setData:(NSData *)data
+		 forKey:(NSData *)aKey
+		  error:(NSError **)error
+   writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Snapshots are read-only");
+
+- (BOOL)deleteDataForKey:(NSData *)aKey NA("Snapshots are read-only");
+- (BOOL)deleteDataForKey:(NSData *)aKey
+				   error:(NSError **)error NA("Snapshots are read-only");
+- (BOOL)deleteDataForKey:(NSData *)aKey
+			writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Snapshots are read-only");
+- (BOOL)deleteDataForKey:(NSData *)aKey
+				   error:(NSError **)error
+			writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Snapshots are read-only");
+
+- (BOOL)performWriteBatch:(void (^)(RocksDBWriteBatch *batch, RocksDBWriteOptions *options))batch NA("Snapshots are read-only");
+- (BOOL)performWriteBatch:(void (^)(RocksDBWriteBatch *batch, RocksDBWriteOptions *options))batch
+					error:(NSError **)error NA("Snapshots are read-only");
+
+- (RocksDBSnapshot *)snapshot NA("Yo Dawg, Snapshot in Snapshot ... ");
 
 @end
