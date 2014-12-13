0c35181eb6f620b0547b7690ddc2c7c665b0affe
iska <iska.gp@gmail.com>
2014-12-13 03:54:52 +0100

    Move all unavailable Snapshot selector declaration into separate header to prevent clutter
    This solution requires extra work every time a new write-operation is introduced to the DB
    superclass, however it beats code duplication.
    
    The alternative are:
    - just ignore the unavailable methods and rely on client code to do the right thing.
    - break the inheritance and let Snapshot redefine/reimplement the needed methods.

--- ObjectiveRocks/RocksDBSnapshot.h
+++ ObjectiveRocks/RocksDBSnapshot.h
@@ -9,6 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "RocksDB.h"
 #import "RocksDBReadOptions.h"
+#import "RocksDBSnapshotUnavailable.h"
 
 namespace rocksdb {
 	class DB;
@@ -20,8 +21,6 @@ namespace rocksdb {
 
 @end
 
-#define NA(x) __attribute__((unavailable(x)))
-
 @interface RocksDBSnapshot (Unavailable)
 
 - (instancetype)initWithPath:(NSString *)path NA("Create a snapshot via a DB instance");
@@ -31,37 +30,11 @@ namespace rocksdb {
 - (void)setDefaultReadOptions:(void (^)(RocksDBReadOptions *readOptions))readOptions
 			  andWriteOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Specify options when creating snapshot via DB instance");
 
-- (BOOL)setData:(NSData *)data
-		 forKey:(NSData *)aKey NA("Snapshots are read-only");
-- (BOOL)setData:(NSData *)data
-		 forKey:(NSData *)aKey
-		  error:(NSError **)error NA("Snapshots are read-only");
-- (BOOL)setData:(NSData *)data
-		 forKey:(NSData *)aKey
-   writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Snapshots are read-only");
-- (BOOL)setData:(NSData *)data
-		 forKey:(NSData *)aKey
-		  error:(NSError **)error
-   writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Snapshots are read-only");
-
-- (BOOL)deleteDataForKey:(NSData *)aKey NA("Snapshots are read-only");
-- (BOOL)deleteDataForKey:(NSData *)aKey
-				   error:(NSError **)error NA("Snapshots are read-only");
-- (BOOL)deleteDataForKey:(NSData *)aKey
-			writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Snapshots are read-only");
-- (BOOL)deleteDataForKey:(NSData *)aKey
-				   error:(NSError **)error
-			writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Snapshots are read-only");
-
-- (BOOL)performWriteBatch:(void (^)(RocksDBWriteBatch *batch, RocksDBWriteOptions *options))batch NA("Snapshots are read-only");
-- (BOOL)performWriteBatch:(void (^)(RocksDBWriteBatch *batch, RocksDBWriteOptions *options))batch
-					error:(NSError **)error NA("Snapshots are read-only");
-- (BOOL)applyWriteBatch:(RocksDBWriteBatch *)writeBatch
-	   withWriteOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Snapshots are read-only");
-- (BOOL)applyWriteBatch:(RocksDBWriteBatch *)writeBatch
-				  error:(NSError **)error
-		   writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions NA("Snapshots are read-only");
-
 - (RocksDBSnapshot *)snapshot NA("Yo Dawg, Snapshot in Snapshot ... ");
+- (RocksDBSnapshot *)snapshotWithReadOptions:(void (^)(RocksDBReadOptions *readOptions))readOptions  NA("Yo Dawg, Snapshot in Snapshot ... ");
+
+#define NA_SELECTOR(sel) sel NA("Snapshot is read-only");
+SNAPSHOT_PUT_MERGE_DELETE_SELECTORS
+#undef NA_SELECTOR
 
 @end
---
+++ ObjectiveRocks/RocksDBSnapshotUnavailable.h
@@ -0,0 +1,46 @@
+//
+//  RocksDBSnapshotUnavailable.h
+//  ObjectiveRocks
+//
+//  Created by Iska on 13/12/14.
+//  Copyright (c) 2014 BrainCookie. All rights reserved.
+//
+
+#define NA(x) __attribute__((unavailable(x)))
+
+#define SNAPSHOT_PUT_MERGE_DELETE_SELECTORS \
+NA_SELECTOR(- (BOOL)setObject:(id)anObject forKey:(id)aKey) \
+NA_SELECTOR(- (BOOL)setObject:(id)anObject forKey:(id)aKey error:(NSError **)error) \
+NA_SELECTOR(- (BOOL)setObject:(id)anObject forKey:(id)aKey writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptionsBlock) \
+NA_SELECTOR(- (BOOL)setObject:(id)anObject forKey:(id)aKey error:(NSError **)error writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+\
+NA_SELECTOR(- (BOOL)setData:(NSData *)data forKey:(NSData *)aKey) \
+NA_SELECTOR(- (BOOL)setData:(NSData *)data forKey:(NSData *)aKey error:(NSError **)error) \
+NA_SELECTOR(- (BOOL)setData:(NSData *)data forKey:(NSData *)aKey writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+NA_SELECTOR(- (BOOL)setData:(NSData *)data forKey:(NSData *)aKey error:(NSError **)error writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+\
+NA_SELECTOR(- (BOOL)mergeObject:(id)anObject forKey:(id)aKey) \
+NA_SELECTOR(- (BOOL)mergeObject:(id)anObject forKey:(id)aKey error:(NSError **)error) \
+NA_SELECTOR(- (BOOL)mergeObject:(id)anObject forKey:(id)aKey writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+NA_SELECTOR(- (BOOL)mergeObject:(id)anObject forKey:(id)aKey error:(NSError **)error writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+\
+NA_SELECTOR(- (BOOL)mergeData:(NSData *)data forKey:(NSData *)aKey) \
+NA_SELECTOR(- (BOOL)mergeData:(NSData *)data forKey:(NSData *)aKey error:(NSError **)error) \
+NA_SELECTOR(- (BOOL)mergeData:(NSData *)data forKey:(NSData *)aKey writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+NA_SELECTOR(- (BOOL)mergeData:(NSData *)data forKey:(NSData *)aKey error:(NSError **)error writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+\
+NA_SELECTOR(- (BOOL)deleteObjectForKey:(id)aKey) \
+NA_SELECTOR(- (BOOL)deleteObjectForKey:(id)aKey error:(NSError **)error) \
+NA_SELECTOR(- (BOOL)deleteObjectForKey:(id)aKey writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+NA_SELECTOR(- (BOOL)deleteObjectForKey:(id)aKey error:(NSError **)error writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+\
+NA_SELECTOR(- (BOOL)deleteDataForKey:(NSData *)aKey) \
+NA_SELECTOR(- (BOOL)deleteDataForKey:(NSData *)aKey error:(NSError **)error) \
+NA_SELECTOR(- (BOOL)deleteDataForKey:(NSData *)aKey writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+NA_SELECTOR(- (BOOL)deleteDataForKey:(NSData *)aKey error:(NSError **)error writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+\
+NA_SELECTOR(- (RocksDBWriteBatch *)writeBatch) \
+NA_SELECTOR(- (BOOL)performWriteBatch:(void (^)(RocksDBWriteBatch *batch, RocksDBWriteOptions *options))batch) \
+NA_SELECTOR(- (BOOL)performWriteBatch:(void (^)(RocksDBWriteBatch *batch, RocksDBWriteOptions *options))batch error:(NSError **)error) \
+NA_SELECTOR(- (BOOL)applyWriteBatch:(RocksDBWriteBatch *)writeBatch withWriteOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
+NA_SELECTOR(- (BOOL)applyWriteBatch:(RocksDBWriteBatch *)writeBatch error:(NSError **)error writeOptions:(void (^)(RocksDBWriteOptions *writeOptions))writeOptions) \
