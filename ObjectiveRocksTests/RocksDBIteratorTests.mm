75074182cd2a466e3dcf6a301cb00b29bb13cc99
iska <iska.gp@gmail.com>
2014-12-13 18:03:08 +0100

    Add Iterator test case with id-based keys/values

--- ObjectiveRocksTests/RocksDBIteratorTests.mm
+++ ObjectiveRocksTests/RocksDBIteratorTests.mm
@@ -154,4 +154,39 @@
 	[iterator close];
 }
 
+- (void)testDB_Iterator_Encoded
+{
+	_rocks = [[RocksDB alloc] initWithPath:_path andDBOptions:^(RocksDBOptions *options) {
+		options.createIfMissing = YES;
+		options.keyEncoder = ^ NSData * (id key) {
+			return [key dataUsingEncoding:NSUTF8StringEncoding];
+		};
+		options.keyDecoder = ^ NSString * (NSData *data) {
+			return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+		};
+		options.valueEncoder = ^ NSData * (id key, id value) {
+			return [value dataUsingEncoding:NSUTF8StringEncoding];
+		};
+		options.valueDecoder = ^ NSString * (id key, NSData * data) {
+			if (data == nil) return nil;
+			return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+		};
+	}];
+
+	[_rocks setObject:@"value 1" forKey:@"Key 1"];
+	[_rocks setObject:@"value 2" forKey:@"Key 2"];
+	[_rocks setObject:@"value 3" forKey:@"Key 3"];
+
+	NSMutableArray *actual = [NSMutableArray array];
+	RocksDBIterator *iterator = [_rocks iterator];
+	[iterator enumerateKeysUsingBlock:^(id key, BOOL *stop) {
+		[actual addObject:key];
+	}];
+
+	NSArray *expected = @[ @"Key 1", @"Key 2", @"Key 3" ];
+	XCTAssertEqualObjects(actual, expected);
+
+	[iterator close];
+}
+
 @end
