34878889ba96e6c8582a80b3a0f07eecd1ac666e
iska <iska.gp@gmail.com>
2015-05-17 03:04:45 +0200

    Add source code documentation for the Env & Thread Status classes

--- ObjectiveRocks/RocksDBEnv.h
+++ ObjectiveRocks/RocksDBEnv.h
@@ -8,14 +8,35 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ All file operations (and other operating system calls) issued by the RocksDB implementation are routed through an
+ `RocksDBEnv` object. Currently `RocksDBEnv` only exposes the high & low priority thread pool parameters.
+ */
 @interface RocksDBEnv : NSObject
 
+/**
+ Initializes a new Env instane with the given high & low priority background threads.
+
+ @param lowPrio The count of thread for the high priority queue.
+ @param highPrio The count of thread for the low priority queue.
+ @return A newly-initialized instance with the given number of threads.
+ */
 + (instancetype)envWithLowPriorityThreadCount:(int)lowPrio andHighPriorityThreadCount:(int)highPrio;
 
+/**  @brief Sets the count of thread for the high priority queue. */
 - (void)setHighPriorityPoolThreadsCount:(int)numThreads;
+
+/**  @brief Sets the count of thread for the low priority queue. */
 - (void)setLowPriorityPoolThreadsCount:(int)numThreads;
 
 #if ROCKSDB_USING_THREAD_STATUS
+/**
+ Returns an array with the status of all threads that belong to the current Env.
+
+ @see RocksDBThreadStatus
+
+ @warning This method is not available in RocksDB Lite.
+ */
 - (NSArray *)threadList;
 #endif
 
--- ObjectiveRocks/RocksDBThreadStatus.h
+++ ObjectiveRocks/RocksDBThreadStatus.h
@@ -8,32 +8,64 @@
 
 #import <Foundation/Foundation.h>
 
+/** The type of a thread. */
 typedef NS_ENUM(int, RocksDBThreadType)
 {
+	/** @brief RocksDB background thread in high priority thread pool. */
 	RocksDBThreadHighPiority = 0,
+
+	/** @brief RocksDB background thread in low priority thread pool. */
 	RocksDBThreadLowPiority,
+
+	/** @brief User thread (Non-RocksDB background thread). */
 	RocksDBThreadUser
 };
 
+/** 
+ The type used to refer to a thread operation. A thread operation describes high-level action of a thread.
+ */
 typedef NS_ENUM(int, RocksDBOperationType)
 {
+	/** Unknown operation. */
 	RocksDBOperationUnknown = 0,
+
+	/** A compaction operation. */
 	RocksDBOperationCompaction,
+
+	/** A flush operation. */
 	RocksDBOperationFlush
 };
 
+/** The type used to refer to a thread state. */
 typedef NS_ENUM(int, RocksDBStateType)
 {
+	/** Unkown state. */
 	RocksDBStateUnknown = 0,
 };
 
+/**
+ Describes the current status of a thread.
+ */
 @interface RocksDBThreadStatus : NSObject
 
+/** @brief An unique ID for the thread. */
 @property (nonatomic, assign) uint64_t threadId;
+
+/** @brief The type of the thread. */
 @property (nonatomic, assign) RocksDBThreadType threadType;
+
+/** @brief The name of the DB instance where the thread is currently involved with. 
+ It would be set to empty string if the thread does not involve in any DB operation. */
 @property (nonatomic, strong) NSString *databaseName;
+
+/** @brief The name of the column family where the thread is currently It would be set 
+ to empty string if the thread does not involve in any column family. */
 @property (nonatomic, strong) NSString *columnFamilyname;
+
+/** @brief The operation (high-level action) that the current thread is involved. */
 @property (nonatomic, assign) RocksDBOperationType operationType;
+
+/** @brief The state (lower-level action) that the current thread is involved. */
 @property (nonatomic, assign) RocksDBStateType stateType;
 
 @end
