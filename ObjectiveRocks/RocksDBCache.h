cb34377145876b54c17836ababfb087719fb6c42
iska <iska.gp@gmail.com>
2015-07-16 23:43:27 +0200

    Add source code documentation for the RocksDB Cache & Filter Policy classes

--- ObjectiveRocks/RocksDBCache.h
+++ ObjectiveRocks/RocksDBCache.h
@@ -8,9 +8,29 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ RocksDB cache.
+ */
 @interface RocksDBCache : NSObject
 
+/**
+ Create a new cache with a fixed size capacity. The cache is sharded
+ to 2^numShardBits shards, by hash of the key. The total capacity
+ is divided and evenly assigned to each shard. The default 
+ numShardBits is 4.
+
+ @see capacity The cache capcity.
+ */
 + (instancetype)LRUCacheWithCapacity:(size_t)capacity;
+
+/**
+ Create a new cache with a fixed size capacity. The cache is sharded
+ to 2^numShardBits shards, by hash of the key. The total capacity
+ is divided and evenly assigned to each shard.
+
+ @see capacity The cache capcity.
+ @see numShardBits The number of shard bits.
+ */
 + (instancetype)LRUCacheWithCapacity:(size_t)capacity numShardsBits:(int)numShardBits;
 
 @end
