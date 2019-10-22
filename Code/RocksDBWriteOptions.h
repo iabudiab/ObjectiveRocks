9ad73842799e001ecb66ee8ce6a5b2930f8f29f4
Jurriaan Mous <>
2019-10-22 19:48:58 +0200

    Expand write options with noSlowdown and lowPriority options

--- Code/RocksDBWriteOptions.h
+++ Code/RocksDBWriteOptions.h
@@ -30,6 +30,21 @@ NS_ASSUME_NONNULL_BEGIN
 */
 @property (nonatomic, assign) BOOL ignoreMissingColumnFamilies;
 
+/** @brief If true and we need to wait or sleep for the write request, fails immediately
+ with [Status.Code.Incomplete].
+ Default: false
+*/
+@property (nonatomic, assign) BOOL noSlowdown;
+
+/** @brief If true, this write request is of lower priority if compaction is
+ behind. In this case that, [.noSlowdown] == true, the request
+ will be cancelled immediately with [Status.Code.Incomplete] returned.
+ Otherwise, it will be slowed down. The slowdown value is determined by
+ RocksDB to guarantee it introduces minimum impacts to high priority writes.
+ Default: false
+*/
+@property (nonatomic, assign) BOOL lowPriority;
+
 @end
 
 NS_ASSUME_NONNULL_END
